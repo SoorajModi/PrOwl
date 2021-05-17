906bec1ac25e742243572745b14178a83e1b2c0c
SoorajModi <soomodi@gmail.com>
2021-05-17 01:05:22 -0400

    Add logging

--- owl/owl.py
+++ owl/owl.py
@@ -1,15 +1,22 @@
+import logging
 from .reddit import stream
 from .scan import is_match
 from .notify import notify
+
+log_format = "%(asctime)s::%(levelname)s::%(name)s::%(filename)s::%(lineno)d::%(message)s"
+logging.basicConfig(level='INFO', format=log_format)
 
 submission_stream = stream()
 
 
 def watch():
     for submission in submission_stream:
-        handle_submission(submission, "owl/scan/keywords.txt", "owl/notify/message.txt")
+        handle_submission(submission)
 
 
-def handle_submission(submission, keywords: str, message: str):
-    if is_match(submission.selftext, keywords):
-        notify(submission, message)
+def handle_submission(submission):
+    logging.info('Scanning submission... {' + submission.title + '}')
+
+    if is_match(submission.selftext, "owl/scan/keywords.txt"):
+        logging.warning("Match found")
+        notify(submission, "owl/notify/message.txt")
