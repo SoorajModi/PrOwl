312a2d9aecb6c7db1e4045438353b5db78b3c880
SoorajModi <soomodi@gmail.com>
2021-05-18 00:26:55 -0400

    Minor housekeeping tasks

--- owl/scan/__init__.py
+++ owl/scan/__init__.py
@@ -5,12 +5,12 @@ Scan text and return if it matches a set of keywords
     Usage example:
 
     from .scan import is_match
-    is_match(submission.selftext, "owl/scan/keywords.txt")
+    contains_keywords(submission.selftext, "owl/scan/keywords.txt")
 
     from .scan import get_keywords
     get_keywords("owl/scan/keywords.txt")
 
 """
 
-from .scan import is_match
+from .scan import contains_keywords
 from .scan import get_keywords
