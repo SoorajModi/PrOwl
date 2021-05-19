dbe6006e7eaeed8759da14037b22a4476c21d994
SoorajModi <soomodi@gmail.com>
2021-05-19 01:10:28 -0400

    Add __main__.py and refactor

--- prowl/__init__.py
+++ prowl/__init__.py
@@ -1,13 +1,10 @@
-"""PrOwl a Reddit watch bot
+"""PrOwl: a Reddit watch bot
 
-This program will watch for submissions on a specified subreddit and notify a user if a post matches
-a set of keywords.
+This application will watch for submissions on a specified subreddit
+and notify a user if a post matches a set of keywords.
 
     Usage example:
 
-    import prowl
-    prowl.watch()
+    python -m prowl
 
 """
-
-from .prowl import watch
---
+++ prowl/__main__.py
@@ -0,0 +1,8 @@
+"""Begin PrOwl
+"""
+
+from .prowl import watch
+
+
+if __name__ == '__main__':
+    watch()
