923f079b1c7f788175aeb370f0d700f173a144ef
Sooraj Modi <soomodi@gmail.com>
2021-05-25 21:56:03 -0400

    Update setup.py to v1.0.0 (#37)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='PrOwl',
-    version='0.1.0',
+    version='1.0.0',
     description='A Reddit Bot',
     long_description=readme,
     url='https://github.com/soorajmodi/prowl',
