49942e9c31ca58fe9df335ae9754908613694e62
Sooraj <soomodi@gmail.com>
2021-09-06 18:41:23 -0400

    Updates (#45)
    * Remove timestamp from message template
    
    * PR template improvements
    
    * Update kerywords

--- prowl/notify/message.py
+++ prowl/notify/message.py
@@ -15,7 +15,6 @@ def generate_message(submission, template: Template) -> str:
     """
 
     return template.substitute(title=submission.title,
-                               time=submission.created_utc,
                                author=submission.author.name,
                                link=submission.url,
                                post=submission.selftext)
