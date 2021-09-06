c58fe28412345bd3b1d8c57b36100ac9bad6e3bf
Sooraj <soomodi@gmail.com>
2021-09-06 17:17:39 -0400

    Author (#44)
    * Add author information to message template
    
    * Fix tests
    
    * Refactor tests

--- test/notify/test_generate_message.py
+++ test/notify/test_generate_message.py
@@ -2,19 +2,26 @@ from string import Template
 from prowl.notify.message import generate_message
 
 
+class MockAuthor(object):
+    def __init__(self, name):
+        self.name = name
+
+
 class MockSubmission(object):
-    def __init__(self, title, time, url, selftext):
+    def __init__(self, title, time, author, url, selftext):
         self.title = title
         self.created_utc = time
+        self.author = MockAuthor(author)
         self.url = url
         self.selftext = selftext
 
 
 def test_generate_message():
-    submission: MockSubmission = MockSubmission('title', 'time', 'url', 'selftext')
-    template: Template = Template('Title: $title\nLink: $link\nPost: $post\n\nHoot hoot <3\n')
+    submission: MockSubmission = MockSubmission('title', 'time', 'author', 'url', 'selftext')
+    template: Template = Template('Title: $title\nTime: $time\nAuthor: $author\nLink: $link\nPost: $post\n\nHoot hoot '
+                                  '<3\n')
 
-    expected: str = "Title: title\nLink: url\nPost: selftext\n\nHoot hoot <3\n"
+    expected: str = "Title: title\nTime: time\nAuthor: author\nLink: url\nPost: selftext\n\nHoot hoot <3\n"
     received: str = generate_message(submission, template)
 
     assert expected == received
